99dabe4f7738fdf4f67b40fe7ed7e9a999dd01c4
HiralChauhan21 <vishal.chauhan.home@gmail.com>
2024-11-10 13:05:55 +0530

    commit

---
+++ main.py
@@ -0,0 +1,21 @@
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("Name: ", name)
+print("The Data Type of name is ", type(name))
+print("Age: ", age)
+print("the data type of age is: ", type(age))
+print("is_student: ", is_student)
+print("the data type of is_student is : ", type(is_student))
+print("weight: ", weight)
+print("the data type of weight is : ", type(weight))
+
+print("\n After Type Casting...")
+age= str(age)
+print(age)
+print("the data type of age is: ", type(age))
+weight = int(weight)
+print(weight)
+print("the data type of weight is : ", type(weight))
